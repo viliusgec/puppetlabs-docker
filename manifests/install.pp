b565bb9e2016f3a8a8ce56265bb9e90fdd92bf84
Vilius Gečas <Vilius.Gecas@adform.com>
2024-02-29 17:05:55 +0200

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -59,19 +59,7 @@ class docker::install (
                 name   => $docker::docker_engine_package_name,
           }))
         }
-        /docker-ce-cli/ : {
-          ensure_resource('package', 'docker-ce-cli', stdlib::merge($docker_hash, {
-                ensure => '20.10.10-3.el7',
-                source => $docker::package_source,
-                name   => $docker::docker_ce_cli_package_name,
-          }))
-        }
         /docker-ce/ : {
-          ensure_resource('package', 'docker-ce-cli', stdlib::merge($docker_hash, {
-                ensure => 'installed',
-                source => $docker::package_source,
-                name   => $docker::docker_ce_cli_package_name,
-          }))
           ensure_resource('package', 'docker', stdlib::merge($docker_hash, {
                 ensure => $ensure,
                 source => $docker::package_source,
